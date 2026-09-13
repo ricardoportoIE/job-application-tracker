f796f78630a4390ca281c76568508bc052897751
Ricardo Porto <ricardoportooliveira@gmail.com>
2026-09-13 10:42:12 +0100

    feat(infra): add ECS service

--- infrastructure/ecs.tf
+++ infrastructure/ecs.tf
@@ -10,3 +10,84 @@ resource "aws_ecs_cluster" "main" {
     Name = "${var.project_name}-${var.environment}-cluster"
   }
 }
+
+resource "aws_ecs_task_definition" "backend" {
+  family                   = "${var.project_name}-${var.environment}-backend"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+
+  cpu    = "256"
+  memory = "512"
+
+  execution_role_arn = aws_iam_role.ecs_task_execution.arn
+
+  container_definitions = jsonencode([
+    {
+      name      = "backend"
+      image     = "${aws_ecr_repository.backend.repository_url}:${var.backend_image_tag}"
+      essential = true
+
+      portMappings = [
+        {
+          containerPort = 8000
+          hostPort      = 8000
+          protocol      = "tcp"
+        }
+      ]
+
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          "awslogs-group"         = aws_cloudwatch_log_group.backend.name
+          "awslogs-region"        = var.aws_region
+          "awslogs-stream-prefix" = "backend"
+        }
+      }
+    }
+  ])
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-backend-task"
+  }
+}
+
+resource "aws_ecs_service" "backend" {
+  name            = "${var.project_name}-${var.environment}-backend"
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.backend.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  health_check_grace_period_seconds = 60
+
+  deployment_circuit_breaker {
+    enable   = true
+    rollback = true
+  }
+  network_configuration {
+    subnets = [
+      aws_subnet.private_a.id,
+      aws_subnet.private_b.id,
+    ]
+
+    security_groups = [
+      aws_security_group.ecs.id,
+    ]
+
+    assign_public_ip = false
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.backend.arn
+    container_name   = "backend"
+    container_port   = 8000
+  }
+
+  depends_on = [
+    aws_lb_listener.http,
+  ]
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-backend-service"
+  }
+}
